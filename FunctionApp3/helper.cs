128253b2615bbaf9a7c8922110580bc9ce38d3dc
SamVivers <sam.vivers@ocsl.co.uk>
2019-10-22 12:49:29 +0100

    re-used code in helper class

---
+++ FunctionApp3/helper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+public class Helper
+{
+    public static async Task<string> GetInfoAsync(string URL, string token)
+    {
+        // send a get request for all resources, with bearer token in header
+        HttpClient httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Remove("Authorization");
+        httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+        HttpResponseMessage response = await httpClient.GetAsync(URL);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public static string FormatResponse(string responseBody)
+    {
+
+        // request responce (parsed to string) is formated (one line per resource) and outputed to a file
+        string body = "";
+        int s = 1;
+        for (int i = 0; i<responseBody.Length - 1; i++)
+        {
+            try
+            {
+
+                if (responseBody.Substring(i, 1) == "[")
+                {
+                    body += "[\n";
+                    s = i + 1;
+                }
+                if (responseBody.Substring(i, 2) == ",{")
+                {
+                    body += responseBody.Substring(s, i - s) + "\n";
+                    s = i + 1;
+                }
+                if (responseBody.Substring(i, 1) == "]")
+                {
+                    body += responseBody.Substring(s, i - s) + "\n";
+                    body += "]";
+                }
+            }
+            catch (ArgumentOutOfRangeException outOfRange)
+            {
+
+                Console.WriteLine("Error: {0}", outOfRange.Message);
+            }
+        }
+        return body;
+    }
+
+    public static void OutputLocal(string body, string date)
+    {
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, $"Resources{date}.txt")))
+        {
+            outputFile.Write(body);
+        }
+    }
+    public static async Task OutputCloudAsync(CloudBlobClient blobClient, string containerName, string fileName, string body)
+    {
+        // Create container and upload file
+        CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+        CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+        using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(body)))
+        {
+            await blockBlob.UploadFromStreamAsync(stream);
+        }
+    }
+}
---
+++ FunctionApp3/resources.cs
@@ -0,0 +1,100 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Collections.Generic;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace FunctionApp3
+{
+    public static class Function1
+    {
+        [FunctionName("resources")]
+        public static async Task RunAsync([TimerTrigger("0 0 4 * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        // environment variables
+            string tenantId = Environment.GetEnvironmentVariable("tenantId");
+            string clientId = Environment.GetEnvironmentVariable("appIdSV");
+            string clientKey = Environment.GetEnvironmentVariable("appKeySV");
+            string subId = Environment.GetEnvironmentVariable("subIdSV");
+            string storageName = Environment.GetEnvironmentVariable("storageName");
+            string storageKey = Environment.GetEnvironmentVariable("storageKey");
+            string containerName = "resources-sv";
+
+        // reformat DateTime, as / and : not usable in filenames
+            // include 'dateFull.Substring(17, 2)' to specify seconds (use 11, 2 for hours and 14, 2 for mins) when run. So that name is unique when running frequently for testing
+            // formated as dd-mm-yyyy
+            string dateFull = DateTime.Now.ToString();
+            string date = dateFull.Substring(3, 2) + "-" + dateFull.Substring(0, 2) + "-" + dateFull.Substring(6, 4);
+
+
+        // define Azure Stroage Account
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=" + storageName + ";AccountKey=" + storageKey + ";EndpointSuffix=core.windows.net";
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+        // aquire bearer token, variable given when creating service principal. Only needs to run once (token is valid for 1 year by default) but currently runs everytime
+            string authContextURL = "https://login.windows.net/" + tenantId;
+            AuthenticationContext authenticationContext = new AuthenticationContext(authContextURL);
+            var credential = new ClientCredential(clientId, clientKey);
+            var result = await authenticationContext
+            .AcquireTokenAsync("https://management.azure.com/", credential);
+            if (result == null)
+            {
+                throw new InvalidOperationException("Failed to obtain the token");
+            }
+            string token = result.AccessToken;
+
+
+            // create and upload All Resources file
+            string responseBody = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resources?api-version=2017-05-10", token);
+
+            string body = Helper.FormatResponse(responseBody);
+
+            await Helper.OutputCloudAsync(blobClient, containerName, date + "-AllResources", body);
+
+
+        // create and upload Resources by resource group files
+            string responseBodyRG = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resourceGroups?api-version=2014-04-01", token);
+
+            // create a List containing name of each resource group
+            List<string> resourceGroupsList = new List<string>();
+            int s = 0;
+            int e = 0;
+            for (int i = 0; i < responseBodyRG.Length - 13; i++)
+            {
+                try
+                {
+                    if (responseBodyRG.Substring(i, 6) == "name\":")
+                    {
+                        s = i + 7;
+                    }
+                    if (responseBodyRG.Substring(i, 10) == "location\":")
+                    {
+                        e = i - 3;
+                        resourceGroupsList.Add(responseBodyRG.Substring(s, e - s));
+                    }
+                }
+                catch (ArgumentOutOfRangeException outOfRange)
+                {
+
+                    Console.WriteLine("Error: {0}", outOfRange.Message);
+                }
+            }
+
+            // output resources with a folder structure, Resource_Groups > resource group name > dated resources file
+            foreach (string resourceGroup in resourceGroupsList)
+            {
+                string responseBodyRGResources = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + Environment.GetEnvironmentVariable("subIdSV") + "/resourceGroups/" + resourceGroup + "/resources?api-version=2017-05-10", token);
+                
+                string bodyRG = Helper.FormatResponse(responseBodyRGResources);
+
+                await Helper.OutputCloudAsync(blobClient, containerName, "ResourceGroups/" + resourceGroup + "/" + date + "-" + resourceGroup, bodyRG);
+            }
+        }
+    }
+}
