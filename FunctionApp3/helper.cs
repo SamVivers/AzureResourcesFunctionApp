e3fc8fde2361ecd82e82a574d7281cee8ced6eb4
VSacr1 <vjsacre@gmail.com>
2019-10-29 09:59:13 +0000

    added comments

--- FunctionApp3/helper.cs
+++ FunctionApp3/helper.cs
@@ -7,6 +7,12 @@ using Microsoft.WindowsAzure.Storage.Blob;
 
 public class Helper
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="URL"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
     public static async Task<string> GetInfoAsync(string URL, string token)
     {
         // send a get request for resources, with bearer token in header
@@ -17,16 +23,22 @@ public class Helper
         return await response.Content.ReadAsStringAsync();
     }
 
+    /// <summary>
+    /// Formats the response by turning the Json format into a easier to read format
+    /// request response (json parsed to string) is formated one line per resource
+    /// Out of bound exception within a try and catch statement.
+    /// </summary>
+    /// <param name="responseBody"></param>
+    /// <returns></returns>
     public static string FormatResponse(string responseBody)
     {
-        // request response (json parsed to string) is formated one line per resource
+        // 
         string body = "";
         int start = 0;
-        for (int i = 0; i<responseBody.Length - 1; i++)
+        for (int i = 0; i < responseBody.Length - 1; i++)
         {
             try
             {
-
                 if (responseBody.Substring(i, 1) == "[")
                 {
                     body += "[\n";
@@ -52,6 +64,12 @@ public class Helper
         return body;
     }
 
+    /// <summary>
+    /// Creates the folder in MyDocuments
+    /// Printing the result from the body string to MyDocuments 
+    /// </summary>
+    /// <param name="body"></param>
+    /// <param name="date"></param>
     public static void OutputLocal(string body, string date)
     {
         string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
@@ -60,9 +78,18 @@ public class Helper
             outputFile.Write(body);
         }
     }
+    /// <summary>
+    /// Creates the Container and Block Blob. 
+    /// Printing the result from the body string to Azure Blob Container. 
+    /// upload file, include / in fileName to create folders
+    /// </summary>
+    /// <param name="blobClient"></param>
+    /// <param name="containerName"></param>
+    /// <param name="fileName"></param>
+    /// <param name="body"></param>
+    /// <returns></returns>
     public static async Task OutputCloudAsync(CloudBlobClient blobClient, string containerName, string fileName, string body)
     {
-        // Create container and upload file, include / in fileName to create folders
         CloudBlobContainer container = blobClient.GetContainerReference(containerName);
         await container.CreateIfNotExistsAsync();
         CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
--- FunctionApp3/resources.cs
+++ FunctionApp3/resources.cs
@@ -9,6 +9,9 @@ using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace FunctionApp3
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class Function1
     {
         [FunctionName("resources")]
@@ -16,7 +19,7 @@ namespace FunctionApp3
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-        // variables
+            // User and Azure container string variables
             string tenantId = Environment.GetEnvironmentVariable("tenantIdCANCOM");
             string clientId = Environment.GetEnvironmentVariable("appIdSV");
             string clientKey = Environment.GetEnvironmentVariable("appKeySV");
@@ -25,19 +28,20 @@ namespace FunctionApp3
             string storageKey = Environment.GetEnvironmentVariable("storageKey");
             string containerName = "resources-sv";
 
-        // reformat DateTime, as / and : not usable in filenames
+            // reformat DateTime, as / and : not usable in filenames
             // include 'dateFull.Substring(17, 2)' to specify seconds (use 11, 2 for hours and 14, 2 for mins) so that fileName is unique when running frequently for testing
             // formated as dd-mm-yyyy
             string dateFull = DateTime.Now.ToString();
             string date = dateFull.Substring(3, 2) + "-" + dateFull.Substring(0, 2) + "-" + dateFull.Substring(6, 4);
 
 
-        // define Azure Stroage Account
+            // define Azure Stroage Account
             string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=" + storageName + ";AccountKey=" + storageKey + ";EndpointSuffix=core.windows.net";
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
 
-        // aquire bearer token, variable given when creating service principal. Only needs to run once (token is valid for 1 year by default) but currently runs everytime function is triggered
+            // aquire bearer token, variable given when creating service principal. Only needs to run once (token is valid for 1 year by default) but currently runs everytime function is triggered
+            // if result is null then it will throw an exception which fails to obtain token. 
             string authContextURL = "https://login.windows.net/" + tenantId;
             AuthenticationContext authenticationContext = new AuthenticationContext(authContextURL);
             var credential = new ClientCredential(clientId, clientKey);
@@ -49,7 +53,7 @@ namespace FunctionApp3
             string token = result.AccessToken;
 
 
-        // create and upload All Resources file
+            // create and upload All Resources file
             string responseBody = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resources?api-version=2017-05-10", token);
 
             string body = Helper.FormatResponse(responseBody);
@@ -57,15 +61,18 @@ namespace FunctionApp3
             await Helper.OutputCloudAsync(blobClient, containerName, date + "-AllResources", body);
 
 
-        // create and upload Resources files by resource group
+            // create and upload Resources files by resource group
             string responseBodyRG = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resourceGroups?api-version=2014-04-01", token);
 
-            // create a List containing name of each resource group
+            // create a List containing name of each resource group 
             List<string> resourceGroupsList = new List<string>();
             int start = 0;
             int end = 0;
+
+
             for (int i = 0; i < responseBodyRG.Length - 12; i++)
             {
+                //Try these if statements and catch the error if the substring is out of range. This will produce a controlled out of range exception error. 
                 try
                 {
                     if (responseBodyRG.Substring(i, 6) == "name\":")
@@ -88,7 +95,7 @@ namespace FunctionApp3
             foreach (string resourceGroup in resourceGroupsList)
             {
                 string responseBodyRGResources = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resourceGroups/" + resourceGroup + "/resources?api-version=2017-05-10", token);
-                
+
                 string bodyRG = Helper.FormatResponse(responseBodyRGResources);
 
                 await Helper.OutputCloudAsync(blobClient, containerName, "ResourceGroups/" + resourceGroup + "/" + date + "-" + resourceGroup, bodyRG);
