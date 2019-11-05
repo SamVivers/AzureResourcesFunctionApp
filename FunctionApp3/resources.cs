b8e52f72f7f342ef64f63f594a2382fdb72990aa
SamVivers <sam.vivers@ocsl.co.uk>
2019-11-05 09:40:22 +0000

    removed 'Helper' class, more split into methods

--- FunctionApp3/resources.cs
+++ FunctionApp3/resources.cs
@@ -6,6 +6,9 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System.Collections.Generic;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.IO;
+using System.Text;
+using System.Net.Http;
 
 namespace FunctionApp3
 {
@@ -16,7 +19,7 @@ namespace FunctionApp3
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-        // variables
+            // variables
             string tenantId = Environment.GetEnvironmentVariable("tenantIdCANCOM");
             string clientId = Environment.GetEnvironmentVariable("appIdSV");
             string clientKey = Environment.GetEnvironmentVariable("appKeySV");
@@ -25,10 +28,40 @@ namespace FunctionApp3
             string storageKey = Environment.GetEnvironmentVariable("storageKey");
             string containerName = "resources-sv";
 
-        // reformat DateTime, as / and : not usable in filenames
-            // include 'dateFull.Substring(17, 2)' to specify seconds (use 11, 2 for hours and 14, 2 for mins) so that fileName is unique when running frequently for testing,
-            // above is true only after the 10th of Oct, Nov, Dec; indices vary as single digit days/months are formatted as 'n' not '0x' in Azure Function Apps
-            // formated as dd-mm-yyyy in Azure Function App, the DateTime struct returns differently formatted result in Visual Studio, Azure CLI and Azure Function Apps :@
+            // reformat DateTime.Now, as / and : not usable in filenames
+            string dateFormatted = FormatDateTime();
+
+            // aquire bearer token for the service principle
+            string token = await AquireBearerTokenAsync(tenantId, clientId, clientKey);
+
+            // define Azure Stroage Account
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=" + storageName + ";AccountKey=" + storageKey + ";EndpointSuffix=core.windows.net";
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // create and upload All Resources file
+            string responseBody = await GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resources?api-version=2017-05-10", token);
+            string body = FormatResponse(responseBody);
+            await OutputCloudAsync(blobClient, containerName, "AllResources" + dateFormatted + ".txt", body);
+
+            // create and upload Resources files by resource group
+            string responseBodyRG = await GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resourceGroups?api-version=2014-04-01", token);
+            List<string> resourceGroupsList = ListResourceGroups(responseBodyRG, log);
+
+            // output resources with a folder structure, ResourceGroups > resource group name > dated resources file
+            foreach (string resourceGroup in resourceGroupsList)
+            {
+                string responseBodyRGResources = await GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resourceGroups/" + resourceGroup + "/resources?api-version=2017-05-10", token);
+                string bodyRG = FormatResponse(responseBodyRGResources);
+                await OutputCloudAsync(blobClient, containerName, "ResourceGroups/" + resourceGroup + "/" + resourceGroup + "Resources" + dateFormatted + ".txt", bodyRG);
+            }
+        }
+
+        // include 'dateFull.Substring(17, 2)' to specify seconds (use 11, 2 for hours and 14, 2 for mins) so that fileName is unique when running frequently for testing,
+        // above is true only after the 10th of Oct, Nov, Dec; indices vary as single digit days/months are formatted as 'n' not '0n' in Azure Function Apps
+        // formated as dd-mm-yyyy in Azure Function App, the DateTime struct returns differently formatted result in Visual Studio, Azure CLI and Azure Function Apps :@
+        public static string FormatDateTime()
+        {
             string dateFull = DateTime.Now.ToString();
             string dateFormatted = "";
             if (dateFull.Substring(1, 1) == "/" && dateFull.Substring(3, 1) == "/")
@@ -47,35 +80,26 @@ namespace FunctionApp3
             {
                 dateFormatted = dateFull.Substring(3, 2) + "-" + dateFull.Substring(0, 2) + "-" + dateFull.Substring(6, 4);
             }
+            return dateFormatted;
+        }
 
-        // define Azure Stroage Account
-            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=" + storageName + ";AccountKey=" + storageKey + ";EndpointSuffix=core.windows.net";
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-        // aquire bearer token, variable given when creating service principal. Only needs to run once (token is valid for 1 year by default) but currently runs everytime function is triggered
+        // aquire bearer token, variables given when creating service principal. Only needs to run once (token is valid for 1 year by default) but currently runs everytime function is triggered
+        public static async Task<string> AquireBearerTokenAsync(string tenantId, string clientId, string clientKey)
+        { 
             string authContextURL = "https://login.windows.net/" + tenantId;
             AuthenticationContext authenticationContext = new AuthenticationContext(authContextURL);
             var credential = new ClientCredential(clientId, clientKey);
             var result = await authenticationContext.AcquireTokenAsync("https://management.azure.com/", credential);
-            if (result == null)
-            {
-                throw new InvalidOperationException("Failed to obtain the token");
-            }
-            string token = result.AccessToken;
+                if (result == null)
+                {
+                    throw new InvalidOperationException("Failed to obtain the token");
+                }
+            return result.AccessToken;
+        }
 
-        // create and upload All Resources file
-            string responseBody = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resources?api-version=2017-05-10", token);
-
-            string body = Helper.FormatResponse(responseBody);
-
-            await Helper.OutputCloudAsync(blobClient, containerName, dateFormatted + "-AllResources", body);
-
-
-        // create and upload Resources files by resource group
-            string responseBodyRG = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resourceGroups?api-version=2014-04-01", token);
-
-            // create a List containing name of each resource group
+        // create a List containing name of each resource group in the target subscription
+        public static List<string> ListResourceGroups(string responseBodyRG, ILogger log)
+        {
             List<string> resourceGroupsList = new List<string>();
             int start = 0;
             int end = 0;
@@ -98,15 +122,74 @@ namespace FunctionApp3
                     log.LogInformation("Error: {0} RG", outOfRange.Message);
                 }
             }
+            return resourceGroupsList;
+        }
 
-            // output resources with a folder structure, ResourceGroups > resource group name > dated resources file
-            foreach (string resourceGroup in resourceGroupsList)
+
+        // send a get request for resources, with bearer token in header
+        public static async Task<string> GetInfoAsync(string URL, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Remove("Authorization");
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+            HttpResponseMessage response = await httpClient.GetAsync(URL);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+
+        // request response (json parsed to string) is formated one line per resource
+        public static string FormatResponse(string responseBody)
+        {
+            string body = "";
+            int start = 0;
+            for (int i = 0; i < responseBody.Length - 1; i++)
             {
-                string responseBodyRGResources = await Helper.GetInfoAsync("https://management.azure.com/subscriptions/" + subId + "/resourceGroups/" + resourceGroup + "/resources?api-version=2017-05-10", token);
+                try
+                {
 
-                string bodyRG = Helper.FormatResponse(responseBodyRGResources);
+                    if (responseBody.Substring(i, 1) == "[")
+                    {
+                        body += "[\n";
+                        start = i + 1;
+                    }
+                    if (responseBody.Substring(i, 2) == ",{")
+                    {
+                        body += responseBody.Substring(start, i - start) + "\n";
+                        start = i + 1;
+                    }
+                    if (responseBody.Substring(i, 1) == "]")
+                    {
+                        body += responseBody.Substring(start, i - start) + "\n";
+                        body += "]";
+                    }
+                }
+                catch (ArgumentOutOfRangeException outOfRange)
+                {
 
-                await Helper.OutputCloudAsync(blobClient, containerName, "ResourceGroups/" + resourceGroup + "/" + dateFormatted + "-" + resourceGroup, bodyRG);
+                    Console.WriteLine("Error: {0} format", outOfRange.Message);
+                }
+            }
+            return body;
+        }
+
+        public static void OutputLocal(string body, string dateFormatted)
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, $"Resources{dateFormatted}.txt")))
+            {
+                outputFile.Write(body);
+            }
+        }
+
+        // Create container and upload file, include / in fileName to create folders
+        public static async Task OutputCloudAsync(CloudBlobClient blobClient, string containerName, string fileName, string body)
+        {
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            await container.CreateIfNotExistsAsync();
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(body)))
+            {
+                await blockBlob.UploadFromStreamAsync(stream);
             }
         }
     }
